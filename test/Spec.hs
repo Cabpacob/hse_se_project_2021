83cfe9eca52966a9f97f3a080554f825b2e8653d
Glebanister <glebmar2001@gmail.com>
2021-11-04 17:26:12 +0300

    Setup HUnit

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+
+test1 = TestCase (assertEqual "s" 1 2)
+
+test2 = TestCase (assertEqual "s" 1 2)
+
+tests =
+  TestList
+    [ TestLabel "test1" test1
+    ]
+
+main = runTestTT tests
