602244500a32b589c54947a43ec74c6e9852a4da
Glebanister <glebmar2001@gmail.com>
2021-11-04 20:09:14 +0300

    Pass one of test

--- src/F451.hs
+++ src/F451.hs
@@ -3,10 +3,9 @@
 module F451 where
 
 import Data.Char (isSpace, toLower)
+import GHC.OldList (find)
 
 newtype Score = Score Float deriving (Eq, Num, Show)
-
-data OneOf a = OneOf[a]
 
 data ScoreConstraints = ScoreConstraints
   { minScore :: Score,
@@ -33,16 +32,20 @@ data StrictEvalParams = StrictEvalParams {trim :: Bool, noCaseSense :: Bool}
 evPs :: Bool -> Bool -> StrictEvalParams
 evPs = StrictEvalParams
 
-doesMatchStrictString :: StrictEvalParams -> String -> String -> Bool
-doesMatchStrictString params corr attempt = process corr == process attempt
+processWithParams :: StrictEvalParams -> String -> String
+processWithParams params = processTrim . processSenseCase
   where
-    process = processTrim . processSenseCase
     processTrim = if trim params then trimStr else idStr
     processSenseCase = if noCaseSense params then fmap toLower else idStr
     trimStr = f . f
       where
         f = reverse . dropWhile isSpace
     idStr s = s
+
+doesMatchStrictString :: StrictEvalParams -> String -> String -> Bool
+doesMatchStrictString params corr attempt = process corr == process attempt
+  where
+    process = processWithParams params
 
 strictStringEval :: StrictEvalParams -> String -> ScoreConstraints -> Maybe String -> Score
 strictStringEval params correct = evalAns (doesMatchStrictString params correct)
@@ -53,12 +56,20 @@ taskStrict params quest corr constr = Task quest (strictStringEval params corr c
 taskStrictDefault :: q -> String -> ScoreConstraints -> Task q String
 taskStrictDefault = taskStrict (StrictEvalParams False False)
 
-taskMultipleStrict :: StrictEvalParams -> q -> OneOf String -> ScoreConstraints -> Task q String
-taskMultipleStrict params quest corr constr = undefined
-
 
 scoreTask :: Eq a => Task q a -> Maybe a -> Score
 scoreTask (Task _ ev) = ev
 
 scoreTasks :: Eq a => Task q a -> [Maybe a] -> [Score]
 scoreTasks (Task _ ev) attList = ev <$> attList
+
+
+newtype OneOf a = OneOf[a]
+
+doesMatchOneOf :: StrictEvalParams -> OneOf String -> String -> Bool
+doesMatchOneOf params (OneOf ops) attempt = f attempt `elem` (f <$> ops)
+  where
+    f = processWithParams params
+
+taskMultipleStrict :: StrictEvalParams -> q -> OneOf String -> ScoreConstraints -> Task q String
+taskMultipleStrict params quest corr constr = Task quest $ evalAns (doesMatchOneOf params corr) constr
