71ade6e2e60162ecdcc654a7092a20c8300a03c6
Glebanister <glebmar2001@gmail.com>
2021-11-16 21:31:55 +0000

    Fix Main.hs to correct file

--- app/Main.hs
+++ app/Main.hs
@@ -1,132 +1,39 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Main where
+import F451
+import Data.List (intercalate)
 
-module F451 where
+questions = [
+    taskQuiz "How many countries still have the shilling as currency?" (OneOf ["1", "2", "4", "0"]) 2 oneZeroConstraints,
+    taskQuiz "How many languages are written from right to left?" (OneOf ["12", "31", "23", "8"]) 0 oneZeroConstraints,
+    taskQuiz "Demolition of the Berlin wall separating East and West Germany began in what year?" (OneOf ["1991", "1985", "1992", "1989"]) 3 oneZeroConstraints,
+    taskQuiz "What is the rarest M&M color?" (OneOf ["Brown", "Yellow", "Blue", "Red"]) 0 oneZeroConstraints
+    ]
 
-import Data.Char (isSpace, toLower)
-import Data.Bool (bool)
-import Data.List (length)
-import Foreign.Marshal.Utils (fromBool)
-import GHC.OldList (find)
-import GHC.Float (int2Float)
+newtype Student a = Student (String, [Maybe a])
 
-newtype Score = Score Float deriving (Eq, Num, Show)
+students = [
+    Student ("Lisa", [Just 2, Nothing , Just 3, Just 0]),
+    Student ("David", [Just 2, Nothing , Nothing, Just 0]),
+    Student ("Stan", [Just 1, Nothing , Just 2, Just 0]),
+    Student ("Alex", [Nothing , Nothing , Just 3, Just 1])
+    ]
 
-data ScoreConstraints = ScoreConstraints
-  { minScore :: Score
-  , noAnswerScore :: Score
-  , maxScore :: Score
-  }
+score :: Score -> Float
+score (Score s) = s
 
-oneZeroConstraints :: ScoreConstraints
-oneZeroConstraints = ScoreConstraints (Score 0) (Score 0) (Score 1)
+evalTaskStudent :: [Task q a] -> Student a -> Float
+evalTaskStudent tasks (Student (_, answers)) = sum $ score . uncurry eval <$> zip tasks answers
 
-data Task q a = Task
-  { question :: q
-  , eval :: Maybe a -> Score
-  }
+printStudentReport :: [Task q a] -> Student a -> [Char]
+printStudentReport tasks s@(Student (name, _)) = name ++ ": " ++ show (evalTaskStudent tasks s)
 
-evalAns :: (t -> Bool) -> ScoreConstraints -> Maybe t -> Score
-evalAns  _        cs  Nothing = noAnswerScore cs
-evalAns doesMatch cs (Just ans)
-  | doesMatch ans = maxScore cs
-  | otherwise     = minScore cs
+printQuestions :: [Task String a] -> String
+printQuestions tasks = intercalate "\n" $ (\(n, t) -> show n ++ ". " ++ question t) <$> zip [1..] tasks
 
-type StringProcessor = String -> String
-type StrProcList = [StringProcessor]
-
-procTrim :: String -> String
-procTrim = f . f
-  where
-     f = reverse . dropWhile isSpace
-
-procToLower :: String -> String
-procToLower = fmap toLower
-
-processWithParams :: [StringProcessor] -> String -> String
-processWithParams = foldl (.) id
-
-doesMatchStrictString :: [StringProcessor] -> String -> String -> Bool
-doesMatchStrictString params corr attempt = process corr == process attempt
-  where
-    process = processWithParams params
-
-strictStringEval :: StrProcList -> String -> ScoreConstraints -> Maybe String -> Score
-strictStringEval params correct = evalAns (doesMatchStrictString params correct)
-
-taskStrict :: StrProcList -> q -> String -> ScoreConstraints -> Task q String
-taskStrict params quest corr constr = Task quest (strictStringEval params corr constr)
-
-taskStrictDefault :: q -> String -> ScoreConstraints -> Task q String
-taskStrictDefault = taskStrict []
-
-scoreTask :: Eq a => Task q a -> Maybe a -> Score
-scoreTask (Task _ ev) = ev
-
-scoreTasks :: Eq a => Task q a -> [Maybe a] -> [Score]
-scoreTasks (Task _ ev) attList = ev <$> attList
-
-newtype OneOf a = OneOf [a]
-
-doesMatchOneOf :: StrProcList -> OneOf String -> String -> Bool
-doesMatchOneOf params (OneOf ops) attempt = f attempt `elem` (f <$> ops)
-  where
-    f = processWithParams params
-
-taskMultipleStrict :: StrProcList -> q -> OneOf String -> ScoreConstraints -> Task q String
-taskMultipleStrict params quest corr constr = Task quest $ evalAns (doesMatchOneOf params corr) constr
-
-doesMatchNothing :: StrProcList -> OneOf String -> String -> Bool
-doesMatchNothing params (OneOf ops) attempt = f attempt `notElem` (f <$> ops)
-  where
-    f = processWithParams params
-
-taskNonMultipleStrict :: StrProcList -> q -> OneOf String -> ScoreConstraints -> Task q String
-taskNonMultipleStrict params quest corr constr = Task quest $ evalAns (doesMatchNothing params corr) constr
-
-newtype Property a = Property (a -> Bool)
-
-taskNumber :: q -> Property Int -> ScoreConstraints -> Task q Int
-taskNumber quest (Property prop) constr = Task quest $ evalAns prop constr
-
-taskQuiz :: q -> OneOf a -> Int -> ScoreConstraints -> Task q Int
-taskQuiz quest vars corr constr = Task quest $ Score . maybe 0 (fromBool . (corr == ))
-
-newtype AllOf a = AllOf [(a, Score)]
-
-sumOfAns :: Eq a => ScoreConstraints -> AllOf a -> Maybe [a] -> Score
-sumOfAns constr _ Nothing 
-    = noAnswerScore constr
-sumOfAns constr (AllOf vars) (Just as) 
-    = Score $ sum (fmap (\ans -> sum $ fmap (\(var, Score scr) -> bool 0.0 scr (var == ans)) vars) as) / int2Float (length vars)
-
-taskMultipleRequired :: Eq a => q -> AllOf a -> ScoreConstraints -> Task q [a]
-taskMultipleRequired quest vars constr = Task quest $ sumOfAns constr vars
-
-mistakes :: String -> String -> Int
-mistakes [] [] = 0
-mistakes (x:xs) (y:ys)
-  | x == y    = mistakes xs ys
-  | otherwise = 1 + mistakes xs ys
-
-taskOneMistake :: q -> String -> ScoreConstraints -> Task q String
-taskOneMistake quest corr constr = Task quest $ Score . maybe 0 (fromBool . (1 ==) . mistakes corr)
-
-taskNMistakes :: q -> String -> Int -> ScoreConstraints -> Task q String
-taskNMistakes quest corr num constr = Task quest $ Score . maybe 0 (fromBool . (num ==) . mistakes corr)
-
-{- ax + b = c С автоматическим вычислением ответа -}
-taskSimpleEquation :: q -> Float -> Float -> Float -> ScoreConstraints -> Task q Float
-taskSimpleEquation quest a b c constr = Task quest $ Score . maybe 0 (\x -> fromBool (abs(a * x + b - c) < eps))
-  where
-    eps = 1e-9
-
-taskNoMoreThanOneMistake :: q -> String -> ScoreConstraints -> Task q String
-taskNoMoreThanOneMistake quest corr constr = Task quest $ Score . maybe 0 (fromBool . (1 >=) . mistakes corr)
-
-taskQuadraticEquation :: q -> Float -> Float -> Float -> ScoreConstraints -> Task q (Float, Float)
-taskQuadraticEquation quest a b c constr = Task quest $ Score . maybe 0 (\(x1, x2) -> fromBool ((abs(a * x1 * x1 + b * x1 + c) < eps) && (abs(a * x2 * x2 + b * x2 + c) < eps)))
-  where
-    eps = 1e-9
-
-taskIntervalChecking :: q -> Int -> Int -> ScoreConstraints -> Task q Int
-taskIntervalChecking quest a b constr = Task quest $ Score . maybe 0 (fromBool . (\x -> a <= x && x <= b))
+main :: IO ()
+main = putStrLn $ unlines [
+    "Today's test: ",
+    printQuestions questions,
+    "Results: ",
+    intercalate "\n" (printStudentReport questions <$> students)
+    ]
